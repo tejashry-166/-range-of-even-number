141535e1d79cd4bfcf8a4d16ea4487f2ce2c15ed
tejashry-166 <71425861+tejashry-166@users.noreply.github.com>
2021-06-28 22:31:33 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+publi class even
+{
+public static vvoid mainn(String args[])
+{
+Scannerr sc=new scanner(System.in);
+int start,end,i;
+System.out.println("enter first range ");
+start=sc.nextInt();
+System.out.println("enter second range ");
+end=sc.nextInt();
+i=end;
+while(i>=start)
+{
+if(i%2==0)
+{
+System.out.println("i");
+}
+i--;
+}
+}
+}
+
+
